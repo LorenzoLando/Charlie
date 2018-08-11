7dffe92657aeb8efb99019f32445f2891bdd95d8
Lorenzo <lorenzo.lando85@gmail.com>
2018-08-11 16:19:55 +0100

    animazione iniziale separata

---
+++ app/assets/scripts/main.js
@@ -0,0 +1,9 @@
+var text = document.getElementsByClassName('text');
+var structure = document.getElementsByClassName('structure');
+
+text.addEventListener('click', function(){
+
+  alert('bella li funziona');
+
+
+});
