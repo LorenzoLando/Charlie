e927a979c485fc83304bfeab4a6be1509f8a3ff6
Lorenzo <lorenzo.lando85@gmail.com>
2018-08-21 23:54:41 +0100

    oggi

--- app/assets/scripts/main.js
+++ app/assets/scripts/main.js
@@ -12,3 +12,28 @@
 
 
 // });
+
+////////////////////////////////////////
+//ANIMAZIONE HEADER CARUSEL
+////////////////////////////////////////
+
+$(function() {
+	$('#carousel').carouFredSel({
+		responsive: true,
+		items: {
+			visible: 1,
+			width: 900,
+			height: 500
+		},
+		scroll: {
+			duration: 250,
+			timeoutDuration: 2500,
+			fx: 'uncover-fade'
+		},
+		pagination: '#pager'
+	});
+});
+
+////////////////////////////////////////
+//FINE ANIMAZIONE HEADER CARUSEL
+////////////////////////////////////////
