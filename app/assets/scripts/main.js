fcb85395509ceb718ef6f6e2ad1c5f26bf5e4e2f
Lorenzo <lorenzo.lando85@gmail.com>
2018-08-25 15:29:34 +0100

    completed header carusel

--- app/assets/scripts/main.js
+++ app/assets/scripts/main.js
@@ -17,6 +17,7 @@
 /////////////////////////////////////////////
 //FINE ANIMAZIONE PER LA COVER IN DISSOLVENZA
 /////////////////////////////////////////////
+
 /////////////////////////////////////////////
 //DISSOLVENZA TITOLO ON SCROLL
 /////////////////////////////////////////////
@@ -26,4 +27,73 @@ $(window).scroll(function(){
 /////////////////////////////////////////////
 //FINE DISSOLVENZA TITOLO ON SCROLL
 /////////////////////////////////////////////
+
+/////////////////////////////////////////////
+//Animazione navbar
+/////////////////////////////////////////////
+
+$(document).ready(function() {
+	$('.toggle-nav').click(function(e) {
+		$(this).toggleClass('active');
+		$('.menu ul').toggleClass('active');
+
+		e.preventDefault();
+	});
+});
+
+//when scrolling down nav background rgb(0,0,128)
+
+
+ $(window).on('scroll', function() {    
+      var scroll = $(window).scrollTop();
+
+      if (scroll >= 500) {
+        
+ 			$('.menu').removeClass('background-top');
+
+ //when scrolling up nav background back to normal
+      } else {
+
+      		$('.menu').addClass('background-top');
+      }
+ });
+/////////////////////////////////////////////
+//FINE animazione navbar
+/////////////////////////////////////////////
+/////////////////////////////////////////////
+//CAROSELLO HEADER
+/////////////////////////////////////////////
+
+  var i = 0; // Start point
+  var images = [];
+  var time = 3000;
+
+  // Image List
+  images[0] = 'assets/images/annie.jpg';
+  images[1] = 'assets/images/josh.jpg';
+  images[2] = 'assets/images/kyaw.jpg';
+  images[3] = 'assets/images/josh.jpg';
+
+  // Change Image
+  function changeImg(){
+    document.slide.src = images[i];
+
+    if(i < images.length - 1){
+      
+      i++;
+    } else {
+      i = 0;
+    }
+
+    setTimeout("changeImg()", time);
+  }
+
+  window.onload = changeImg;
+
+  /////////////////////////////////////////////
+/////////////////////////////////////////////
+// FINE CAROSELLO HEADER
+/////////////////////////////////////////////
+
+
 
